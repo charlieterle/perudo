36e34cce73253bf06c3f3cf256f14a8ee754f37c
Charles Dieterle <cdieterle33@gmail.com>
2020-08-26 09:26:38 -0500

    Continue work on simulation_two, still incomplete

--- simulation_two.py
+++ simulation_two.py
@@ -2,92 +2,112 @@
 # Perudo simulator (no human players)
 # See rules.txt for an English language explanation of the rules
 
-
 import perudo
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 
-def single_game(player_count):
+def simulator(player_count, num_trials, num_intervals):
     """
-    Simulate a single game of Perudo and returns related data
+    Plot statistics on dudo calls based on multiple simulated games of Perudo
 
     Arguments:
         player_count (int) - number of players
+        num_trials (int) - number of games to be simulated
+        num_intervals (int) - determines how many intervals to include on the
+            horizontal axis of the plots.
 
-    Returns:
-        dudos - list of bools (True for successful dudo call, False for failure)
-        dice_counts - list of ints (total number of dice in play in the game)
-        betting_cups - list of ints (num of dice in the betting player's cup)
-        previous_cups - list of ints (num of dice in the previous player's cup)
+    Note: All plots will have a horizontal axis related to the players'
+    individual dice counts and the game's total dice count
     """
 
     my_game = perudo.Game(player_count)
-    dudos, dice_counts, previous_cups, betting_cups = [], [], [], []
+    dice_per_player = perudo.DICE_PER_PLAYER
+
+    # this is the min possible ratio of one's own dice to total dice in play.
+    min_ratio = 1 / (dice_per_player * (player_count - 1) + 1)
+
+    # this is the max possible ratio of one's own dice to total dice in play.
+    max_ratio = dice_per_player/(dice_per_player + 1)
+
+    ratio_diff = max_ratio - min_ratio
+    step = ratio_diff / num_intervals
+
+    # The data will be divided according to player's dice to total dice ratios.
+    # Therefore, it's necessary to convert the ratios into bucket numbers to
+    # populate the lists of data.
+    def get_bucket(ratio):
+        """
+        Get the appropriate bucket number from a given ratio
+
+        If the ratio is min_ratio, the bucket is 0, and if it is max_ratio,
+        the bucket is num_intervals - 1. The range of each bucket is step, which
+        was declared above.
+        """
+        if ratio = max_ratio:
+            return num_intervals - 1
+        else:
+            return int((ratio - min_ratio) / step)
+
+    # The lists below will each have num_intervals lists (buckets), into which
+    # the predicted rates of dudo and actual dudo outcomes will be placed,
+    # based on the ratio of dice in the player's cup to the total number of
+    # dice in play.
+    previous_predicted, previous_actual = [], []
+    current_predicted, current_actual = [], []
+
+    for i in range(num_intervals):
+        previous_predicted.append([])
+        previous_actual.append([])
+        current_predicted.append([])
+        current_actual.append([])
 
     # start the simulation
-    previous_player = None
-    while my_game.players_left() > 1:
-        betting_player = my_game.current_player
-        bet_cup_len = len(my_game.players[betting_player].cup)
-        if previous_player != None:
-            prev_cup_len = len(my_game.players[previous_player].cup)
-        safest_move = my_game.make_safest_move()
-
-        # when dudo is called, add data to the lists declared above
-        if my_game.current_bet == None:
-            if betting_player != my_game.current_player or  \
-                                my_game.players[previous_player].cup == []:
-                dudos.append(True)
-            else:
-                dudos.append(False)
-
-            dice_counts.append(my_game.dice_count)
-            betting_cups.append(bet_cup_len)
-            previous_cups.append(prev_cup_len)
-
-            previous_player = None
-
-        # in this case, dudo wasn't called, so just continue to the next turn
-        else:
-            previous_player = betting_player
-
-    return dudos, dice_counts, betting_cups, previous_cups
-
-def simulator(player_count, num_trials):
-    """
-    Simulate multiple games of Perudo
-
-    Arguments:
-        player_count (int) - number of players in the game
-        num_trials (int) - number of games simulated
-
-    Return the following statistics:
-        - Avg fraction of dudo calls that were correct
-        - Avg number of bets per round
-        - Avg number of rounds per game
-    """
-
-    dudos, dice_counts, previous_cups, betting_cups = [], [], [], []
-
     for i in range(num_trials):
-        one_game = single_game(player_count)
-        dudos.extend(one_game[0])
-        dice_counts.extend(one_game[1])
-        betting_cups.extend(one_game[2])
-        previous_cups.extend(one_game[3])
+        previous_player = None
+        while my_game.players_left() > 1:
+            current_player = my_game.current_player
+            current_cup_len = len(my_game.players[betting_player].cup)
+            if previous_player != None:
+                previous_cup_len = len(my_game.players[previous_player].cup)
 
-    # this is the minimum ratio of one's own dice to total dice in play.
-    # this occurs when the player has 1 die left, and everyone else has 5
-    min_ratio = 1 / (perudo.DICE_PER_PLAYER * player_count - 4)
+            safest_move = my_game.make_safest_move()
 
-    # this is the maximum ratio of one's own dice to total dice in play.
-    # this occurs when the player has 5 dice, and the only remaining opponent
-    # has 1 die left
-    max_ratio = 5/6
+            # when dudo is called, add data to the lists declared above
+            if my_game.current_bet == None:
+                total_dice_count = my_game.dice_count
+                previous_ratio = previous_cup_len / total_dice_count
+                current_ratio = current_cup_len / total_dice_count
+
+                previous_bucket = get_bucket(previous_ratio)
+                current_bucket = get_bucket(current_ratio)
+                #   TODO FROM HERE
+                if betting_player != my_game.current_player or  \
+                                    my_game.players[previous_player].cup == []:
+                    append somehting 1
+                else:
+                    append somethi 0
+                dice_counts.append(my_game.dice_count)
+                betting_cups.append(betting_cup_len)
+                previous_cups.append(previous_cup_len)
+                probs.append(safest_move) # note: safest_move is the calculated
+                                        # probability of a dudo call succeeding
+
+                previous_player = None
+
+            # in this case, dudo wasn't called, so just go to the next turn
+            else:
+                previous_player = betting_player
+
+    betting_ratios
+
+
+    for i in range(len(dudos)):
+
+
 
 
 
 # change the arguments below to desired player count and number of games
-simulator(6, 10)
+simulator(6, 100, 50)
 
 
 # basic usage of pyplot
